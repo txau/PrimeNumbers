21ef0712f6562c7475cb9d5027ba02bf9e06aecb
txau <jaumec@gmail.com>
2013-09-16 16:30:38 -0500

    Added project files and tests.

---
+++ ConsoleApplication3/PrimeNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    public class PrimeNumbers
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        public static List<int> generate(int number)
+        {
+            List<int> result = new List<int>();
+            decomposeNumber(number, result);
+
+            return result;
+        }
+
+        private static void decomposeNumber(int number, List<int> result)
+        {
+            for (int factor = 2; factor <= number; factor++)
+            {
+                var isExactDivision = (number % factor) == 0;
+                if (isExactDivision)
+                {
+                    result.Add(factor);
+                    var divisionResult = number / factor;
+                    decomposeNumber(divisionResult, result);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/PrimeNumbersTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace PrimeNumbers
+{
+    [TestFixture]
+    class TestPrimeNumbers
+    {
+        [Test]
+        public void Generate_On2_Returns2()
+        {
+            List<int> result = PrimeNumbers.generate(2);
+
+            var expected = new List<int> { 2 };
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Genereate_On3_Returns3()
+        {
+            List<int> result = PrimeNumbers.generate(3);
+
+            var expected = new List<int> { 3 };
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Genereate_On4_Returns2_2()
+        {
+            List<int> result = PrimeNumbers.generate(4);
+
+            var expected = new List<int> { 2, 2 };
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Genereate_On8_Returns2_2_2()
+        {
+            List<int> result = PrimeNumbers.generate(8);
+
+            var expected = new List<int> { 2, 2, 2 };
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Genereate_On121_Returns11_11()
+        {
+            List<int> result = PrimeNumbers.generate(121);
+
+            var expected = new List<int> { 11, 11};
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Genereate_On122_Returns2_61()
+        {
+            List<int> result = PrimeNumbers.generate(122);
+
+            var expected = new List<int> { 2, 61 };
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
